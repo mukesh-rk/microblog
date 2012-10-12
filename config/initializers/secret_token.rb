453191d226197e26a179b17d3d90af3b2b22e86a
mukesh-rk <mukesh@rubykitchen.org>
2012-10-12 12:28:43 +0530

    initial commit. Added users model and validations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microblog::Application.config.secret_token = '8138ccec50d4f12f38d3a07bc444e8b5bdced7859c60f9f9c9c77efbf9b4ad72bcc212a15b2b88c72ce6df5f507d5e5571bbe84cdc31ee313a1c7217e4ad3358'
