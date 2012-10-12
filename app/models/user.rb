a957e16bcdb981313f9136e4267c7f9c2073add6
mukesh-rk <mukesh@rubykitchen.org>
2012-10-12 17:07:18 +0530

    added authentication and session(needs testing). finalized Gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,27 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation, :password_digest
   has_secure_password
-
+  before_save :create_remember_token
   before_save { |user| user.email = email.downcase }
-
+  before_save :hash
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: trueend
+  validates :password_confirmation, presence: true
+  
+  def hash
+   	 password_digest= BCrypt::Password.create("#{password}")
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
+
+    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Microblog::Application.routes.draw do
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
   get "users/new"
+
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +66,7 @@ Microblog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'staticpages#home'
 
   # See how all your routes lay out with "rake routes"
 
